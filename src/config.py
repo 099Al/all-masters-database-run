9939b94c59b005159f9cd134a8ced3c026dcec3e
al <walkmore@yandex.ru>
2025-12-30 23:44:08 +0500

    config

--- src/config.py
+++ src/config.py
@@ -7,12 +7,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    HOST: str
-    PORT: str
-    USER: str
-    PASS: str
-    DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
     ENGINE: str
+    POOL_ENGINE: str
+
+    TOKEN_ID: str
+    BOT_ID: str
     GPT_KEY: str
 
 
@@ -23,7 +27,7 @@ class Settings(BaseSettings):
 
     @property
     def connect_url(self):
-        return f'{self.ENGINE}://{self.USER}:{self.PASS}@{self.HOST}:{self.PORT}/{self.DB}'
+        return f'{self.ENGINE}://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}'
 
 
 
